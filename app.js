854c2b3df804ce7d1ce5dfcd22d45cbc5398788d
vmpuri <vpuri@ufl.edu>
2021-06-15 17:04:41 -0400

    Add pretrained chlorophyll content neural network exported to tflite

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
-const app = express()
+var fs = require('fs');
+var path = require('path');
+
+var jsonPath = path.join(__dirname, '.', 'config', 'dev', 'foobar.json');
+var jsonString = fs.readFileSync(jsonPath, 'utf8');
 const port = 3000
 
+const app = express()
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Pong')
 })
 
-app.get('/chl_latest', (req, res) => {
-    res.sendFile('C:/projects/minispect/models/chl.py')
+app.get('/chl-latest', (req, res) => {
+    res.sendFile('./models/chl_nn_minispect.tflite')
 })
 
 app.listen(port, () => {
